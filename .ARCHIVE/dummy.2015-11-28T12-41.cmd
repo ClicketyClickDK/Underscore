385ce1bda08fc04a3713ac813c749af5955b9f64
Erik Bachmann <Erik@Clicketyclick.dk>
2015-11-28 13:01:06 +0100

    2015-11-28
    Remove escaped tags like {PCT}

---
+++ .ARCHIVE/dummy.2015-11-28T12-41.cmd
@@ -0,0 +1,11 @@
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION&::(Don't pollute the global environment with the following)
+::**********************************************************************
+SET $NAME=checkInArchive
+SET $DESCRIPTION=Input routine for batch using VBScript to provide input box
+SET $Author=Erik Bachmann, ClicketyClick.dk [ErikBachmann@ClicketyClick.dk]
+SET $Source=c:\Users\Erik\Documents\GitHub\Underscore\checkInArchive.cmd
+:: History
+::SET $VERSION=xx.xxx&SET $REVISION=YYYY-MM-DDThh:mm:ss&SET $COMMENT=Init / Description
+::SET $VERSION=01.000&SET $REVISION=2010-10-13T15:36:00&SET $COMMENT=ebp / Initial: FindInPath
+  SET $VERSION=01.002&SET $REVISION=2014-01-11T10:59:00&SET $Comment=Update doc + example/EBP
---
+++ .ARCHIVE/getLastBootupTime.2015-11-10T08-35.cmd
@@ -0,0 +1,86 @@
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION&::(Don't pollute the global environment with the following)
+::**********************************************************************
+SET $NAME=%~n0
+SET $DESCRIPTION=Get latest boot up time for system
+SET $AUTHOR=Erik Bachmann, ClicketyClick.dk [ErikBachmann@ClicketyClick.dk]
+SET $SOURCE=%~f0
+::@(#)NAME
+::@(-)  The name of the command or function, followed by a one-line description of what it does.
+::@(#)      %$NAME% -- %$DESCRIPTION%
+::@(#) 
+::@(#)SYNOPSIS
+::@(-)  In the case of a command, a formal description of how to run it and what command line options it takes. 
+::@(-)  For program functions, a list of the parameters the function takes and which header file contains its definition.
+::@(-)  
+::@(#)      %$NAME%
+::@(#) 
+::@(#)OPTIONS
+::@(-)  Flags, parameters, arguments (NOT the Monty Python way)
+::@(#)  -h      Help page
+::@(#)
+::@ (#) 
+::@(#)DESCRIPTION
+::@(-)  A textual description of the functioning of the command or function.
+::@(#) 
+::@(#) 
+::@(#)EXAMPLES
+::@(-)  Some examples of common usage.
+::@(#)     getLastBootupTime.cmd
+::@(#)     DTS=20150328015443.064271+060
+::@(#) 
+::@ (#)EXIT STATUS
+::@(-)  Exit status / errorlevel is 0 if OK, otherwise 1+.
+::@ (#)
+::@ (#)ENVIRONMENT
+::@(-)  Variables affected
+::@ (#)
+::@ (#)
+::@ (#)FILES, 
+::@(-)  Files used, required, affected
+::@ (#)
+::@ (#)
+::@ (#)BUGS / KNOWN PROBLEMS
+::@(-)  If any known
+::@ (#)
+::@ (#)
+::@(#)REQUIRES
+::@(-)  Dependecies
+::@(#)  _Debug.cmd      Setting up debug environment for batch scripts 
+::@(#)  _GetOpt.cmd     Parse command line options and create environment vars
+::@(#) 
+::@ (#)SEE ALSO
+::@(-)  A list of related commands or functions.
+::@ (#)  
+::@ (#)  
+::@ (#)REFERENCE
+::@(-)  References to inspiration, clips and other documentation
+::@ (#)  Author:
+::@ (#)  URL: 
+::@ (#) 
+::@(#)
+::@(#)SOURCE
+::@(-)  Where to find this source
+::@(#)  %$Source%
+::@(#)
+::@ (#)AUTHOR
+::@(-)  Who did what
+::@ (#)  %$AUTHOR%
+::*** HISTORY **********************************************************
+::SET $VERSION=YYYY-MM-DD&SET $REVISION=hh:mm:ss&SET $COMMENT=Description/init
+::SET $VERSION=2015-02-19&SET $REVISION=16:00:00&SET $COMMENT=Autoupdate / ErikBachmann
+::SET $VERSION=2015-03-30&SET $REVISION=10:12:00&SET $COMMENT=Example / ErikBachmann
+  SET $VERSION=2015-10-08&SET $REVISION=11:20:00&SET $COMMENT=GetOpt: Calling usage on -h and exit on error / ErikBachmann
+::**********************************************************************
+::@(#)(c)%$Version:~0,4% %$Author%
+::**********************************************************************
+
+    CALL "%~dp0\_DEBUG"
+    CALL "%~dp0\_Getopt" %*&IF ERRORLEVEL 1 EXIT /B 1
+
+::ENDLOCAL
+
+    FOR /f %%a IN ('WMIC OS GET LastBootUpTime ^| FIND "."') DO SET DTS=%%a
+    SET DTS
+
+::*** End of File *****************************************************
